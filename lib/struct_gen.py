ea68548a7b6b739be0b7f02a5e53066f39ca04f6
Thannozzz <arnabmajumdar9@gmail.com>
2023-10-11 14:41:22 +0200

    update

--- lib/struct_gen.py
+++ lib/struct_gen.py
@@ -2,6 +2,8 @@ import numpy as np
 
 
 ## 3d structures ##
+
+# generates node and connectivity matrix (used in ball in box version)
 def struct_gen_3d (a, b, c, nx, ny, nz): 
     #a = length_a, b = length_b c = length_c
     #nx = no. of divisions in x direction (same for ny and nz)
@@ -26,6 +28,54 @@ def struct_gen_3d (a, b, c, nx, ny, nz):
             cur=cur+1
         cur=cur+1+nz
     return coords, con_3d
+
+
+# generates nodes, connectivity and cells (used in the node_cell version)
+def node_cell_gen_3d (a, b, c, nx, ny, nz): 
+    #a = length_a, b = length_b c = length_c
+    #nx = no. of divisions in x direction (same for ny and nz)
+    
+    # node generation
+    dx=a/(nx)
+    dy=b/(ny)
+    dz=c/(nz)
+    num_nodes=(nx+1)*(ny+1)*(nz+1)
+    num_cells=nx*ny*nz
+    nodes=np.zeros((num_nodes,3))
+    idx=0
+    for i in range(nx+1):
+        for j in range(ny+1):
+            for k in range(nz+1):
+                nodes[idx,:]=np.array([i*dx, j*dx, k*dx])
+                idx+=1
+                #print(idx)
+    
+    # connectovity generation
+    el=np.array([0, 1, nz+2, nz+1, (nz+1)*(ny+1), (nz+1)*(ny+1)+1, \
+                  (nz+1)*(ny+1)+ny+2, (nz+1)*(ny+1)+ny+1])
+    con_3d=np.zeros((num_cells,len(el)), dtype=int)
+    cur=0
+    idx=0
+    for i in range(nx):
+        for j in range(ny):
+            for k in range(nz):
+                con_3d[idx,:]=cur+el
+                #con_3d.append(list((cur)+el))
+                idx+=1
+                cur=cur+1
+            cur=cur+1
+        cur=cur+1+nz
+    
+    # cell middle points
+    cells=np.zeros((num_cells,3))
+    idx=0
+    for connec in con_3d:
+        points=nodes[connec]
+        cell_center=np.average(points, axis=0)
+        cells[idx]=cell_center
+        idx+=1
+        #print(cell_center)
+    return nodes, con_3d, cells
 
 ## 2d structures ##
 def struct_gen_2d (a, b, nx, ny, flat=0): 
