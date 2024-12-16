adb4a721f1bd0e4c3d4ef39040db8f14ff8cf5f1
Arnab Majumdar <arnabmajumdar9@gmail.com>
2024-12-16 12:24:03 +0100

    added 3 cut at 3 different z for generating gif

--- src/sim_gen.py
+++ src/sim_gen.py
@@ -121,7 +121,9 @@ model_param_dir_name=model_param_dir_name[0:-1]
 model_param_dir=os.path.join(model_dir,model_param_dir_name)
 os.makedirs(model_dir, exist_ok=True)
 
-images=[]
+images_1=[] # cut at z = 1/4 * z_max
+images_2=[] # cut at z = 2/4 * z_max (2/4 = 1/2)
+images_3=[] # cut at z = 3/4 * z_max
 for i in range(len(t_arr)):
     t=t_arr[i]
     # create time_dir
@@ -138,21 +140,56 @@ for i in range(len(t_arr)):
         run simulation
         """
         sld=sim.model_run(sim_model, nodes, mid_point, t)
-        ### plottig
-        sld_3d=sld.reshape(nx+1, ny+1, nz+1)
-        nodes_3d=nodes.reshape(nx+1, ny+1, nz+1, 3)
-        # .T is required to exchange x and y axis 
-        # origin is 'lower' to put it in lower left corner 
-        plt.imshow(sld_3d[:,:,(nz+1)//2].T, extent=[0, 20, 0, 20], origin='lower')
-        plot_file=os.path.join(ensem_dir,'snap.jpg')
-        plt.colorbar()
-        plt.title(' time = {0:0>3}s \n emsemble step = {1:0>3} '.format(t,idx_ensem+1))
-        plt.savefig(plot_file, format='jpg')
-        
 
+        # plottig
+        sld_3d=sld.reshape(nx+1, ny+1, nz+1)
+        
+        ## z = 1/4 * z_max
+        nodes_3d=nodes.reshape(nx+1, ny+1, nz+1, 3)
+        z_val=nodes_3d[0, 0, (nz+1)//4, 2]
+        ### .T is required to exchange x and y axis 
+        ### origin is 'lower' to put it in lower left corner 
+        plt.imshow(sld_3d[:,:,(nz+1)//4].T, extent=[0, 20, 0, 20], origin='lower')
+        plot_file_1=os.path.join(ensem_dir,'snap_z_{}.jpg'.format(z_val))
+        plt.colorbar()
+        plt.title(' time = {0:0>3}s \n emsemble step = {1:0>3} \
+            \n z = {2}$\AA$'.format(t,idx_ensem+1,z_val))
+        plt.savefig(plot_file_1, format='jpg', bbox_inches='tight')
         ### add images of ensemble 1 for video
         if idx_ensem==0:  
-            images.append(imageio.imread(plot_file))
+            images_1.append(imageio.imread(plot_file_1))
+            plt.show()
+        
+        ## z = 1/2 * z_max
+        nodes_3d=nodes.reshape(nx+1, ny+1, nz+1, 3)
+        z_val=nodes_3d[0, 0, (nz+1)//2, 2]
+        ### .T is required to exchange x and y axis 
+        ### origin is 'lower' to put it in lower left corner 
+        plt.imshow(sld_3d[:,:,(nz+1)//2].T, extent=[0, 20, 0, 20], origin='lower')
+        plot_file_2=os.path.join(ensem_dir,'snap_z_{}.jpg'.format(z_val))
+        plt.colorbar()
+        plt.title(' time = {0:0>3}s \n emsemble step = {1:0>3} \
+            \n z = {2}$\AA$'.format(t,idx_ensem+1,z_val))
+        plt.savefig(plot_file_2, format='jpg', bbox_inches='tight')
+        ### add images of ensemble 1 for video
+        if idx_ensem==0:  
+            images_2.append(imageio.imread(plot_file_2))
+            plt.show()
+
+        ## z = 2/4 * z_max
+        nodes_3d=nodes.reshape(nx+1, ny+1, nz+1, 3)
+        z_val=nodes_3d[0, 0, 3*(nz+1)//4, 2]
+        ### .T is required to exchange x and y axis 
+        ### origin is 'lower' to put it in lower left corner 
+        plt.imshow(sld_3d[:,:,3*(nz+1)//4].T, extent=[0, 20, 0, 20], origin='lower')
+        plot_file_3=os.path.join(ensem_dir,'snap_z_{}.jpg'.format(z_val))
+        plt.colorbar()
+        plt.title(' time = {0:0>3}s \n emsemble step = {1:0>3} \
+            \n z = {2}$\AA$'.format(t,idx_ensem+1,z_val))
+        plt.savefig(plot_file_3, format='jpg', bbox_inches='tight')
+        ### add images of ensemble 1 for video
+        if idx_ensem==0:  
+            images_3.append(imageio.imread(plot_file_3))
             plt.show()
         """
         save data
@@ -163,4 +200,9 @@ for i in range(len(t_arr)):
         sim_data['sld']=sld
         sim_data.close()
 # save simulation video
-imageio.mimsave(os.path.join(model_dir,'simu.gif'), images, fps=2, loop=0)
+## z = 1/4 * z_max 
+imageio.mimsave(os.path.join(model_param_dir,'simu_1_4.gif'), images_1, fps=2, loop=0)
+## z = 2/4 * z_max 
+imageio.mimsave(os.path.join(model_param_dir,'simu_2_4.gif'), images_2, fps=2, loop=0)
+## z = 3/4 * z_max 
+imageio.mimsave(os.path.join(model_param_dir,'simu_3_4.gif'), images_3, fps=2, loop=0)
