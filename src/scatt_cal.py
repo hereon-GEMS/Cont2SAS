a11e75686e6a193c06326920e1d0c3bb0acbad6b
Arnab Majumdar <arnabmajumdar9@gmail.com>
2024-12-18 15:38:24 +0100

    moved all xml gen files for sassena from src to lib

--- src/scatt_cal.py
+++ src/scatt_cal.py
@@ -133,7 +133,7 @@ model_dir_name= (sim_model + '_tend_' + str(t_end) + '_dt_' + str(dt) \
     + '_ensem_' + str(n_ensem)).replace('.','p')
 model_dir=os.path.join(sim_dir,model_dir_name)
 
-# folder name for run of model with particular run param
+# folder name for model with particular run param
 model_xml=os.path.join(xml_folder, 'model_'+sim_model + '.xml')
 tree = ET.parse(model_xml)
 root = tree.getroot()
@@ -196,121 +196,13 @@ for i in range(len(t_arr)):
         ### pdb dcd generation ###
         pdb_dcd_dir=os.path.join(scatt_dir,'pdb_dcd')
         os.makedirs(pdb_dcd_dir, exist_ok=True)
-        points=np.float32(pseudo_pos)
-        cat_prop=np.float32(pseudo_b_cat_val)
-        topo=md.Topology() #= md.Topology()
-        ch=topo.add_chain()
-        res=topo.add_residue('RES', ch)
-        # os.makedirs(dir_name, exist_ok=True)
-        pdb_file_name=os.path.join(pdb_dcd_dir, 'sample.pdb')
-        for i in range(len(points)):
-            el_name='Pseudo'+str(pseudo_b_cat_idx[i])
-            sym='P'+str(pseudo_b_cat_idx[i])
-            try:
-                ele=(md.element.Element(10,el_name, sym, 10, 10))
-            except AssertionError:
-                ele=(md.element.Element.getBySymbol(sym))
-            topo.add_atom(sym, ele, res)
-        with md.formats.PDBTrajectoryFile(pdb_file_name,'w') as f:
-            f.write(points, topo) 
-        dcd_file_name=os.path.join(pdb_dcd_dir, 'sample.dcd')
-        with md.formats.DCDTrajectoryFile(dcd_file_name, 'w') as f:
-            n_frames = 1
-            for j in range(n_frames):
-                f.write(points)
+        scatt.pdb_dcd_gen(pdb_dcd_dir, pseudo_pos, pseudo_b_cat_val, pseudo_b_cat_idx)
 
         ### database generator ###
-        # org command
-        # dsv.database_generator(min(sld_dyn_cell_cat), max(sld_dyn_cell_cat), ndiv=10, database_dir=db_dir)
-        # detailed version
         db_dir_name='database'
         db_dir=os.path.join(scatt_dir,db_dir_name)
         os.makedirs(db_dir, exist_ok=True)
-
-        b_val=np.unique(pseudo_b_cat_val)
-        b_cat=np.unique(pseudo_b_cat_idx)
-        cur_num_cat=len(b_cat)
-        # copy xml files that are not reproducible by coding 
-        # this can be part of further work
-        # check files in database_sassena dir
-        print('cp -r ../database_sassena/*.xml ' + db_dir + '/')
-        os.system('cp -r ../database_sassena/*.xml ' + db_dir + '/')
-        definition_dir=os.path.join(db_dir, 'definitions')
-        os.makedirs(definition_dir, exist_ok=True)
-    
-        # exclusionfactors-neutron.xml
-        filename='exclusionfactors-neutron.xml'
-        xml_file=os.path.join(definition_dir, filename)
-        
-        exclusionfactors = ET.Element("exclusionfactors")
-        for i in range(cur_num_cat):
-            element = ET.SubElement(exclusionfactors, "element")
-            el_name='Pseudo'+str(b_cat[i])
-            ET.SubElement(element, "name").text = el_name
-            ET.SubElement(element, "type").text = "1"
-            ET.SubElement(element, "param").text = "1"   
-            tree = ET.ElementTree(exclusionfactors)
-        tree.write(xml_file)
-    
-        # masses.xml
-        filename='masses.xml'
-        xml_file=os.path.join(definition_dir, filename)
-    
-        masses = ET.Element("masses")
-        for i in range(cur_num_cat):
-            element = ET.SubElement(masses, "element")
-            el_name='Pseudo'+str(b_cat[i])
-            ET.SubElement(element, "name").text = el_name
-            ET.SubElement(element, "param").text = "1"  
-            tree = ET.ElementTree(masses)
-        tree.write(xml_file)
-        
-        # names.xml
-        filename='names.xml'
-        xml_file=os.path.join(definition_dir, filename)
-    
-        names = ET.Element("names")
-        pdb = ET.SubElement(names, "pdb")
-        for i in range(cur_num_cat):
-            element = ET.SubElement(pdb, "element")
-            el_name='Pseudo'+str(b_cat[i])
-            el_sym='P'+str(b_cat[i])
-            ET.SubElement(element, "name").text = el_name
-            ET.SubElement(element, "param").text = '^ *'+el_sym+'.*'  
-            # ET.SubElement(element, "param").text = '/ '+el_sym+'/g'  
-            tree = ET.ElementTree(names)
-        tree.write(xml_file)
-
-        # sizes.xml
-        filename='sizes-neutron.xml'
-        xml_file=os.path.join(definition_dir, filename)
-    
-        sizes = ET.Element("sizes")
-        for i in range(cur_num_cat):
-            element = ET.SubElement(sizes, "element")
-            el_name='Pseudo'+str(b_cat[i])
-            # el_sym='P'+str(i)
-            ET.SubElement(element, "name").text = el_name
-            ET.SubElement(element, "type").text = '1'
-            ET.SubElement(element, "param").text = '1'  
-            tree = ET.ElementTree(sizes)
-        tree.write(xml_file)
-    
-
-        # scatterfactors-neutron-coherent.xml
-        filename='scatterfactors-neutron-coherent.xml'
-        xml_file=os.path.join(definition_dir, filename)
-        
-        root = ET.Element("scatterfactors")
-        for i in range(cur_num_cat):
-            element = ET.SubElement(root, "element")
-            el_name='Pseudo'+str(b_cat[i])
-            # el_sym='P'+str(i)
-            ET.SubElement(element, "name").text = el_name
-            ET.SubElement(element, "type").text = '0'
-            ET.SubElement(element, "param").text = str(b_val[i]) 
-            tree = ET.ElementTree(root)
-        tree.write(xml_file)
+        scatt.db_gen(db_dir, pseudo_b_cat_val, pseudo_b_cat_idx)
         
         ### scatter.xml generate ###
         # original command 
@@ -318,75 +210,9 @@ for i in range(len(t_arr)):
         # detailed version
         scatter_xml_file_name='scatter.xml'
         scatter_xml_file=os.path.join(scatt_dir,scatter_xml_file_name)
-        
-        
-
-        # filename=os.path.join(dir_name,'scatter.xml')
-        # if os.path.exists(filename):
-        #     shutil.rmtree(filename)
-        pdb_file=os.path.join('pdb_dcd','sample.pdb')
-        dcd_file=os.path.join('pdb_dcd','sample.dcd')
-        # signal_file='signal.h5'
-        # resolution_num=100
-        # scan_vector=[1,0,0]
-        # start_length=0.0
-        # end_length=1.0
-        # num_points=100
-    
-        root=ET.Element("root")
-    
-        #tier 1 subelements
-        
-        sample = ET.SubElement(root, "sample")
-        database = ET.SubElement(root, "database")
-        scattering = ET.SubElement(root, "scattering")
-        limits = ET.SubElement(root, "limits")
-    
-        #sample
-        
-        structure = ET.SubElement(sample, "structure")
-        ET.SubElement(structure, "file").text = pdb_file
-        ET.SubElement(structure, "format").text = 'pdb'
-        framesets = ET.SubElement(sample, "framesets")
-        frameset = ET.SubElement(framesets, "frameset")
-        ET.SubElement(frameset, "file").text = dcd_file
-        ET.SubElement(frameset, "format").text = 'dcd'
-    
-        #database
-        
-        ET.SubElement(database, "file").text = 'database/db-neutron-coherent.xml'
-    
-        #scattering
-        
-        ET.SubElement(scattering, "type").text = 'all'
-        dsp = ET.SubElement(scattering, "dsp")
-        ET.SubElement(dsp, "type").text = 'square'
-        signal = ET.SubElement(scattering, "signal")
-        ET.SubElement(signal, "file").text = signal_file
-        vectors = ET.SubElement(scattering, "vectors")
-        ET.SubElement(vectors, "type").text = 'scans'
-        scans= ET.SubElement(vectors, "scans")
-        scan= ET.SubElement(scans, "scan")
-        ET.SubElement(scan, "X").text = str(scan_vector[0])
-        ET.SubElement(scan, "Y").text = str(scan_vector[1])
-        ET.SubElement(scan, "Z").text = str(scan_vector[2])
-        ET.SubElement(scan, "from").text = str(start_length)
-        ET.SubElement(scan, "to").text = str(end_length)
-        ET.SubElement(scan, "points").text = str(num_points)
-        average = ET.SubElement(scattering, "average")
-        orientation = ET.SubElement(average, "orientation")
-        ET.SubElement(orientation, "type").text = 'vectors'
-        vectors = ET.SubElement(orientation, "vectors")
-        ET.SubElement(vectors, "type").text = 'sphere'
-        ET.SubElement(vectors, "algorithm").text = 'boost_uniform_on_sphere'
-        ET.SubElement(vectors, "resolution").text = str(resolution_num)
-    
-        #limits
-        decomposition=ET.SubElement(limits, "decomposition")
-        ET.SubElement(decomposition, "utilization").text = '0.5'
-    
-        tree = ET.ElementTree(root)
-        tree.write(scatter_xml_file)
+        scatt.scattxml_gen(scatter_xml_file, signal_file,scan_vector,
+                            start_length, end_length, num_points,
+                              resolution_num)
 
         # ### sassena runner ###
         parent_dir=os.getcwd()
