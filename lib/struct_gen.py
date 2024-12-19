63a7567ccfdf1c5f0a00f8b6d1206c188fada157
Arnab Majumdar <arnabmajumdar9@gmail.com>
2024-12-19 16:29:12 +0100

    code clean

--- lib/struct_gen.py
+++ lib/struct_gen.py
@@ -1,35 +1,6 @@
 import numpy as np
 
 
-## 3d structures ##
-
-# generates node and connectivity matrix (used in ball in box version)
-def struct_gen_3d (a, b, c, nx, ny, nz): 
-    #a = length_a, b = length_b c = length_c
-    #nx = no. of divisions in x direction (same for ny and nz)
-    el=np.array([0, 1, nz+2, nz+1, (nz+1)*(ny+1), (nz+1)*(ny+1)+1, \
-                 (nz+1)*(ny+1)+ny+2, (nz+1)*(ny+1)+ny+1])
-    dx=a/(nx)
-    dy=b/(ny)
-    dz=c/(nz)
-    coords=[]
-    for i in range(nx+1):
-        for j in range(ny+1):
-            for k in range(nz+1):
-                coords.append([i*dx, j*dx, k*dx])
-                
-    con_3d=[]
-    cur=0
-    for i in range(nx):
-        for j in range(ny):
-            for k in range(nz):
-                con_3d.append(list((cur)+el))
-                cur=cur+1
-            cur=cur+1
-        cur=cur+1+nz
-    return coords, con_3d
-
-
 # generates nodes, connectivity and cells (used in the node_cell version)
 def node_cell_gen_3d (a, b, c, nx, ny, nz): 
     #a = length_a, b = length_b c = length_c
