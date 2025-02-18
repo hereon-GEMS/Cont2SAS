bdea30416057101bddc24d93ec0e6877fa60edfe
Arnab Majumdar <arnabmajumdar9@gmail.com>
2025-02-18 15:28:01 +0100

    lagrangian and computation threads

--- lib/scatt_cal.py
+++ lib/scatt_cal.py
@@ -8,16 +8,159 @@ import xml.etree.ElementTree as ET
 import os
 import mdtraj as md
 
+# lagrangian function first order
+def L1(x,x1,x2):
+    """
+    x = postion in side element
+    x1 = position of shape function node
+    x2 = position of other node
+    """
+    return (x-x2)/(x1-x2)
 
-def pseudo_b(node_sld,connec,cell_vol):
-    num_node=len(node_sld)
-    num_cell=len(connec)
-    pseudo_b=np.zeros(num_cell)
-    for i in range(num_cell):
-        cell_i_node_idx = connec[i,:]
-        cell_i_node_sld = node_sld[cell_i_node_idx]
-        cell_i_cell_sld = np.average(cell_i_node_sld)
-        pseudo_b[i] = cell_vol*cell_i_cell_sld
+# lagrangian function second order
+def L2(x,x1,x2,x3):
+    """
+    x = postion in side element
+    x1 = position of shape function node
+    x2, x3 = position of other node
+    """
+    return ((x-x2)*(x-x3))/((x1-x2)*(x1-x3))
+
+def pseudo_b(nodes,cells,node_sld,connec,cell_vol,el_info):
+    el_type=el_info['type']
+    el_order=el_info['order']
+    if el_type=='lagrangian':
+        if el_order==1:
+            num_node=len(node_sld)
+            num_cell=len(connec)
+            pseudo_b=np.zeros(num_cell)
+            for i in range(num_cell):
+                cell_i_node_idx = connec[i,:]
+                cell_i_nodes = nodes[cell_i_node_idx,:]
+                cell_i_node_sld = node_sld[cell_i_node_idx]
+                cell_i=cells[i,:]
+                """
+                0 - 1,1,1
+                1 - 1,1,2
+                2 - 1,2,1
+                3 - 1,2,2
+                4 - 2,1,1
+                5 - 2,1,2
+                6 - 2,2,1
+                7 - 2,2,2
+                """
+                p_111=cell_i_nodes[0,:]
+                p_112=cell_i_nodes[1,:]
+                p_121=cell_i_nodes[2,:]
+                p_122=cell_i_nodes[3,:]
+                p_211=cell_i_nodes[4,:]
+                p_212=cell_i_nodes[5,:]
+                p_221=cell_i_nodes[6,:]
+                p_222=cell_i_nodes[7,:]
+                cell_i_cell_sld = (L1(cell_i[0],p_111[0],p_211[0]) * L1(cell_i[1],p_111[1],p_121[1]) * L1(cell_i[2],p_111[2],p_112[2]) * cell_i_node_sld[0] +
+                                   L1(cell_i[0],p_112[0],p_212[0]) * L1(cell_i[1],p_112[1],p_122[1]) * L1(cell_i[2],p_112[2],p_111[2]) * cell_i_node_sld[1] +
+                                   L1(cell_i[0],p_121[0],p_221[0]) * L1(cell_i[1],p_121[1],p_111[1]) * L1(cell_i[2],p_121[2],p_122[2]) * cell_i_node_sld[2] +
+                                   L1(cell_i[0],p_122[0],p_222[0]) * L1(cell_i[1],p_122[1],p_112[1]) * L1(cell_i[2],p_122[2],p_121[2]) * cell_i_node_sld[3] +
+                                   L1(cell_i[0],p_211[0],p_111[0]) * L1(cell_i[1],p_211[1],p_221[1]) * L1(cell_i[2],p_211[2],p_212[2]) * cell_i_node_sld[4] +
+                                   L1(cell_i[0],p_212[0],p_112[0]) * L1(cell_i[1],p_212[1],p_222[1]) * L1(cell_i[2],p_212[2],p_211[2]) * cell_i_node_sld[5] +
+                                   L1(cell_i[0],p_221[0],p_121[0]) * L1(cell_i[1],p_221[1],p_211[1]) * L1(cell_i[2],p_221[2],p_222[2]) * cell_i_node_sld[6] +
+                                   L1(cell_i[0],p_222[0],p_122[0]) * L1(cell_i[1],p_222[1],p_212[1]) * L1(cell_i[2],p_222[2],p_221[2]) * cell_i_node_sld[7])
+                #cell_i_cell_sld = np.average(cell_i_node_sld)
+                pseudo_b[i] = cell_vol*cell_i_cell_sld
+        elif el_order==2:
+            num_node=len(node_sld)
+            num_cell=len(connec)
+            pseudo_b=np.zeros(num_cell)
+            for i in range(num_cell):
+                cell_i_node_idx = connec[i,:]
+                cell_i_nodes = nodes[cell_i_node_idx,:]
+                cell_i_node_sld = node_sld[cell_i_node_idx]
+                cell_i=cells[i,:]
+                """
+                0 - 1,1,1
+                1 - 1,1,2
+                2 - 1,1,3
+                3 - 1,2,1
+                4 - 1,2,2
+                5 - 1,2,3
+                6 - 1,3,1
+                7 - 1,3,2
+                8 - 1,3,3
+                9 - 2,1,1
+                10- 2,1,2
+                11- 2,1,3
+                12- 2,2,1
+                13- 2,2,2
+                14- 2,2,3
+                15- 2,3,1
+                16- 2,3,2
+                17- 2,3,3
+                18- 3,1,1
+                19- 3,1,2
+                20- 3,1,3
+                21- 3,2,1
+                22- 3,2,2
+                23- 3,2,3
+                24- 3,3,1
+                25- 3,3,2
+                26- 3,3,3
+                """
+                p_111=cell_i_nodes[0,:]
+                p_112=cell_i_nodes[1,:]
+                p_113=cell_i_nodes[2,:]
+                p_121=cell_i_nodes[3,:]
+                p_122=cell_i_nodes[4,:]
+                p_123=cell_i_nodes[5,:]
+                p_131=cell_i_nodes[6,:]
+                p_132=cell_i_nodes[7,:]
+                p_133=cell_i_nodes[8,:]
+                p_211=cell_i_nodes[9,:]
+                p_212=cell_i_nodes[10,:]
+                p_213=cell_i_nodes[11,:]
+                p_221=cell_i_nodes[12,:]
+                p_222=cell_i_nodes[13,:]
+                p_223=cell_i_nodes[14,:]
+                p_231=cell_i_nodes[15,:]
+                p_232=cell_i_nodes[16,:]
+                p_233=cell_i_nodes[17,:]
+                p_311=cell_i_nodes[18,:]
+                p_312=cell_i_nodes[19,:]
+                p_313=cell_i_nodes[20,:]
+                p_321=cell_i_nodes[21,:]
+                p_322=cell_i_nodes[22,:]
+                p_323=cell_i_nodes[23,:]
+                p_331=cell_i_nodes[24,:]
+                p_332=cell_i_nodes[25,:]
+                p_333=cell_i_nodes[26,:]
+                cell_i_cell_sld = (L2(cell_i[0],p_111[0],p_211[0],p_311[0]) * L2(cell_i[1],p_111[1],p_121[1],p_131[1]) * L2(cell_i[2],p_111[2],p_112[2],p_113[2]) * cell_i_node_sld[0] +
+                                   L2(cell_i[0],p_112[0],p_212[0],p_312[0]) * L2(cell_i[1],p_112[1],p_122[1],p_132[1]) * L2(cell_i[2],p_112[2],p_111[2],p_113[2]) * cell_i_node_sld[1] +
+                                   L2(cell_i[0],p_113[0],p_213[0],p_313[0]) * L2(cell_i[1],p_113[1],p_123[1],p_133[1]) * L2(cell_i[2],p_113[2],p_111[2],p_112[2]) * cell_i_node_sld[2] +
+                                   L2(cell_i[0],p_121[0],p_221[0],p_321[0]) * L2(cell_i[1],p_121[1],p_111[1],p_131[1]) * L2(cell_i[2],p_121[2],p_122[2],p_123[2]) * cell_i_node_sld[3] +
+                                   L2(cell_i[0],p_122[0],p_222[0],p_322[0]) * L2(cell_i[1],p_122[1],p_112[1],p_132[1]) * L2(cell_i[2],p_122[2],p_121[2],p_123[2]) * cell_i_node_sld[4] +
+                                   L2(cell_i[0],p_123[0],p_223[0],p_323[0]) * L2(cell_i[1],p_123[1],p_113[1],p_133[1]) * L2(cell_i[2],p_123[2],p_121[2],p_122[2]) * cell_i_node_sld[5] +
+                                   L2(cell_i[0],p_131[0],p_231[0],p_331[0]) * L2(cell_i[1],p_131[1],p_111[1],p_123[1]) * L2(cell_i[2],p_131[2],p_132[2],p_133[2]) * cell_i_node_sld[6] +
+                                   L2(cell_i[0],p_132[0],p_232[0],p_332[0]) * L2(cell_i[1],p_132[1],p_112[1],p_122[1]) * L2(cell_i[2],p_132[2],p_131[2],p_133[2]) * cell_i_node_sld[7] +
+                                   L2(cell_i[0],p_133[0],p_233[0],p_333[0]) * L2(cell_i[1],p_133[1],p_113[1],p_123[1]) * L2(cell_i[2],p_133[2],p_131[2],p_132[2]) * cell_i_node_sld[8] +
+                                   L2(cell_i[0],p_211[0],p_111[0],p_311[0]) * L2(cell_i[1],p_211[1],p_221[1],p_231[1]) * L2(cell_i[2],p_211[2],p_212[2],p_213[2]) * cell_i_node_sld[9] +
+                                   L2(cell_i[0],p_212[0],p_112[0],p_312[0]) * L2(cell_i[1],p_212[1],p_222[1],p_232[1]) * L2(cell_i[2],p_212[2],p_211[2],p_213[2]) * cell_i_node_sld[10] +
+                                   L2(cell_i[0],p_213[0],p_113[0],p_313[0]) * L2(cell_i[1],p_213[1],p_223[1],p_233[1]) * L2(cell_i[2],p_213[2],p_211[2],p_212[2]) * cell_i_node_sld[11] +
+                                   L2(cell_i[0],p_221[0],p_121[0],p_321[0]) * L2(cell_i[1],p_221[1],p_211[1],p_231[1]) * L2(cell_i[2],p_221[2],p_222[2],p_223[2]) * cell_i_node_sld[12] +
+                                   L2(cell_i[0],p_222[0],p_122[0],p_322[0]) * L2(cell_i[1],p_222[1],p_212[1],p_232[1]) * L2(cell_i[2],p_222[2],p_221[2],p_223[2]) * cell_i_node_sld[13] +
+                                   L2(cell_i[0],p_223[0],p_123[0],p_323[0]) * L2(cell_i[1],p_223[1],p_213[1],p_233[1]) * L2(cell_i[2],p_223[2],p_221[2],p_222[2]) * cell_i_node_sld[14] +
+                                   L2(cell_i[0],p_231[0],p_131[0],p_331[0]) * L2(cell_i[1],p_231[1],p_211[1],p_223[1]) * L2(cell_i[2],p_231[2],p_232[2],p_233[2]) * cell_i_node_sld[15] +
+                                   L2(cell_i[0],p_232[0],p_132[0],p_332[0]) * L2(cell_i[1],p_232[1],p_212[1],p_222[1]) * L2(cell_i[2],p_232[2],p_231[2],p_233[2]) * cell_i_node_sld[16] +
+                                   L2(cell_i[0],p_233[0],p_133[0],p_333[0]) * L2(cell_i[1],p_233[1],p_213[1],p_223[1]) * L2(cell_i[2],p_233[2],p_231[2],p_232[2]) * cell_i_node_sld[17] +
+                                   L2(cell_i[0],p_311[0],p_111[0],p_211[0]) * L2(cell_i[1],p_311[1],p_321[1],p_331[1]) * L2(cell_i[2],p_311[2],p_312[2],p_313[2]) * cell_i_node_sld[18] +
+                                   L2(cell_i[0],p_312[0],p_112[0],p_212[0]) * L2(cell_i[1],p_312[1],p_322[1],p_332[1]) * L2(cell_i[2],p_312[2],p_311[2],p_313[2]) * cell_i_node_sld[19] +
+                                   L2(cell_i[0],p_313[0],p_113[0],p_213[0]) * L2(cell_i[1],p_313[1],p_323[1],p_333[1]) * L2(cell_i[2],p_313[2],p_311[2],p_312[2]) * cell_i_node_sld[20] +
+                                   L2(cell_i[0],p_321[0],p_121[0],p_221[0]) * L2(cell_i[1],p_321[1],p_311[1],p_331[1]) * L2(cell_i[2],p_321[2],p_322[2],p_323[2]) * cell_i_node_sld[21] +
+                                   L2(cell_i[0],p_322[0],p_122[0],p_222[0]) * L2(cell_i[1],p_322[1],p_312[1],p_332[1]) * L2(cell_i[2],p_322[2],p_321[2],p_323[2]) * cell_i_node_sld[22] +
+                                   L2(cell_i[0],p_323[0],p_123[0],p_223[0]) * L2(cell_i[1],p_323[1],p_313[1],p_333[1]) * L2(cell_i[2],p_323[2],p_321[2],p_322[2]) * cell_i_node_sld[23] +
+                                   L2(cell_i[0],p_331[0],p_131[0],p_231[0]) * L2(cell_i[1],p_331[1],p_311[1],p_323[1]) * L2(cell_i[2],p_331[2],p_332[2],p_333[2]) * cell_i_node_sld[24] +
+                                   L2(cell_i[0],p_332[0],p_132[0],p_232[0]) * L2(cell_i[1],p_332[1],p_312[1],p_322[1]) * L2(cell_i[2],p_332[2],p_331[2],p_333[2]) * cell_i_node_sld[25] +
+                                   L2(cell_i[0],p_333[0],p_133[0],p_233[0]) * L2(cell_i[1],p_333[1],p_313[1],p_323[1]) * L2(cell_i[2],p_333[2],p_331[2],p_332[2]) * cell_i_node_sld[26])
+                #cell_i_cell_sld = np.average(cell_i_node_sld)
+                pseudo_b[i] = cell_vol*cell_i_cell_sld
     return pseudo_b
 
 def pseudo_b_cat(pseudo_b,num_cat,method='extend'):
@@ -265,8 +408,16 @@ def scattxml_gen(scatter_xml_file, signal_file,scan_vector, start_length, end_le
     ET.SubElement(vectors, "resolution").text = str(resolution_num)
 
     #limits
-    decomposition=ET.SubElement(limits, "decomposition")
-    ET.SubElement(decomposition, "utilization").text = '0.5'
+    # decomposition=ET.SubElement(limits, "decomposition")
+    # ET.SubElement(decomposition, "utilization").text = '0.5'
+
+    # stage memory data
+    stage=ET.SubElement(limits, "stage")
+    memory=ET.SubElement(stage, "memory")
+    ET.SubElement(memory, "data").text=str(800000000)
+    # threads
+    computation=ET.SubElement(limits, "computation")
+    ET.SubElement(computation, "threads").text=str(2)
 
     tree = ET.ElementTree(root)
     tree.write(scatter_xml_file)
