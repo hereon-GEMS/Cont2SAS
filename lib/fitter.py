cbf28ec246ba50727db00cacd7487e711695069d
Arnab Majumdar <arnabmajumdar9@gmail.com>
2025-02-18 15:29:06 +0100

    edit

--- lib/fitter.py
+++ lib/fitter.py
@@ -25,13 +25,15 @@ import math
 from lib import analytical as ana
 import xml.etree.ElementTree as ET
 
-def fitter(Iq_num, q_num, model_xml, t_end, t):
+def fitter(Iq_num, q_num, model_xml, t_arr, t_idx, 
+           fit_param_1, fit_param_2):
+    t=t_arr[t_idx]
+    t_end=np.max(t_arr)
     model=model_xml[-6:-4]
     if model=='gg':
+        # read model xml
         tree=ET.parse(model_xml)
         root = tree.getroot()
-        # decreitization params
-        # number of categories and method of categorization
         ball_sld=float(root.find('sld_in').text)
         box_sld=float(root.find('sld_out').text)
         sld_grain=ball_sld-box_sld
@@ -41,27 +43,59 @@ def fitter(Iq_num, q_num, model_xml, t_end, t):
         ball_rad_fit=fitter_gg(Iq_num, q_num, sld_grain)
         ball_rad_sim=r_0+((r_end-r_0)/t_end)*t
         print('ball radius simulation: {0}, fit: {1}'.format(ball_rad_sim, ball_rad_fit))
-        return ball_rad_fit
+        fit_param_1[t_idx]=ball_rad_fit
+        return fit_param_1, fit_param_2
     elif model=='fs':
         print('fitting fuzzy sphere')
-        #fitter_fs()
+        tree=ET.parse(model_xml)
+        root = tree.getroot()
+        ball_sld=float(root.find('sld_in').text)
+        box_sld=float(root.find('sld_out').text)
+        sld_grain=ball_sld-box_sld
+        ball_rad=float(root.find('rad').text)
+        sig_0=float(root.find('sig_0').text)
+        sig_end=float(root.find('sig_end').text)
+        sig_fit, ball_rad_fit=fitter_fs(Iq_num, q_num, sld_grain)
+        sig_sim=sig_0+((sig_end-sig_0)/t_end)*t
+        print('sigma simulation: {0}, fit: {1}'.format(sig_sim, sig_fit))
+        print('radius simulation: {0}, fit: {1}'.format(ball_rad, ball_rad_fit))
+        fit_param_1[t_idx]=sig_fit
+        fit_param_2[t_idx]=ball_rad_fit
+        return fit_param_1, fit_param_2
     else:
         print('not correct model')
            
 def fitter_gg(Iq_num, q_num, sld_grain):
-    def fit_func(q_in, rad_opt):
+    def fit_func_gg(q_in, rad_opt):
         Iq, q_out =ana.ball(qmax=max(q_in),qmin=min(q_in),
                             Npts=len(q_in), scale=1 , bg=0,
                             sld=sld_grain, sld_sol=0,
                             rad=rad_opt)
         return Iq
-    popt, pcov = curve_fit(fit_func, q_num, Iq_num)
+    popt, pcov = curve_fit(fit_func_gg, q_num, Iq_num)
     # Iq_fit=fit_func(q_num, *popt_gg)
     rad_fit=popt[0]
     return rad_fit
+
+def fitter_fs(Iq_num, q_num, sld_grain):
+    def fit_func(q_in, sig_opt, rad_opt):
+        Iq, q_out =ana.fuzzysph(qmax=max(q_in),qmin=min(q_in),
+                            Npts=len(q_in), scale=1 , bg=0,
+                            sld=sld_grain, sld_sol=0,
+                            sig_fuzz=sig_opt, radius=rad_opt)
+        return Iq
+    popt, pcov = curve_fit(fit_func, q_num, Iq_num)
+    # Iq_fit=fit_func(q_num, *popt)
+    # plt.loglog(q_num, Iq_num)
+    # plt.loglog(q_num, Iq_fit)
+    # plt.show()
+    # print('ball radius simulation: {0}, fit: {1}'.format(10, popt[1]))
+    sig_fit=round(np.abs(popt[0]),2)
+    ball_rad_fit=round(popt[1],2)
+    return sig_fit, ball_rad_fit
 
 def get_fit_param(model):
     if model=='gg':
         return 'radius'
     elif model=='fs':
-        return 'fuzz_value'
+        return ['fuzz_value', 'radius']
