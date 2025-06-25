5d6bd998ee77751a5c59639dfe2ec85673425c83
Arnab Majumdar <arnabmajumdar9@gmail.com>
2025-06-25 11:01:04 +0200

    added qclean and clean code

--- bib_ecc_gen.py
+++ bib_ecc_gen.py
@@ -1,7 +1,19 @@
+"""
+This generates data for ball offcenter in box model
+data is saved in data folder
+
+Author: Arnab Majumdar
+Date: 24.06.2025
+"""
 from lib import xml_gen
 import subprocess
 import numpy as np
 import os
+import time
+
+# ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
 
 """
 input values
@@ -26,29 +38,30 @@ plt_node=False
 plt_cell=False
 plt_mesh=False
 
-### sim_gen ###
+### sim gen ###
 sim_model='bib_ecc'
-t_end=0
 dt=1
+t_end=0
 n_ensem=1
 
 ### model_param ###
 rad=10
 sld_in=2
 sld_out=1
-ecc_vec=np.array([0, 0, 0])
+ecc_vec=np.array([5, 5, 5])
+qclean_sld=0
 
 ### scatt_cal ###
-num_cat=5
+num_cat=3
 method_cat='extend'
 sassena_exe= '/home/amajumda/Documents/Softwares/sassena/compile/sassena'
-mpi_procs=2
+mpi_procs=4
 num_threads=2
 sig_file='signal.h5'
 scan_vec=np.array([1, 0, 0])
 Q_range=np.array([0, 1])
 num_points=100
-num_orientation=300
+num_orientation=200
 
 ### run_vars ###
 struct_gen_run=1
@@ -58,10 +71,14 @@ scatt_cal_run=1
 """
 xml gen and run scripts
 """
-part_str='Part>>>'
-info_str='Info>>>'
+part_str='Part>>>  '
+info_str='Info>>>  '
 
-print(part_str + 'Structure generation')
+print(f'-------------------------------------------------')
+print(part_str + 'Generating mesh')
+print('')
+# time counter start - struct gen
+tic_sg = time.perf_counter()
 if struct_gen_run==1:
     # generate xml for struct_gen
     print(info_str + 'Generating struct_gen.xml')
@@ -78,8 +95,17 @@ if struct_gen_run==1:
     subprocess.run(["python", script_path], cwd=working_dir, stderr=subprocess.PIPE)
 else:
     print(info_str + 'structure generation not attempted')
+# time counter end - struct gen 
+toc_sg = time.perf_counter()
+ttot_sg= round(toc_sg - tic_sg,3)
+print(f'Time taken for generating mesh: {ttot_sg} s')
+print(f'-------------------------------------------------')
 
-print(part_str + 'Simulating')
+print(f'-------------------------------------------------')
+print(part_str + 'Assigning SLD values to nodes')
+print('')
+# time counter start - simulation gen
+tic_sim = time.perf_counter()
 if sim_run==1:
     # simulation xml
     print(info_str + 'Generating simulation.xml')
@@ -87,7 +113,7 @@ if sim_run==1:
 
     # model xml
     print(info_str + 'Generating model_bib_ecc.xml')
-    xml_gen.model_bib_ecc_xml_write(xml_dir, rad, sld_in, sld_out, ecc_vec)
+    xml_gen.model_bib_ecc_xml_write(xml_dir, rad, sld_in, sld_out, ecc_vec, qclean_sld)
 
     # generate structure
     print(info_str + 'Executing sim_gen.py')
@@ -96,8 +122,17 @@ if sim_run==1:
     subprocess.run(["python", script_path], cwd=working_dir, stderr=subprocess.PIPE)
 else:
     print(info_str + 'simulation not attempted')
+# time counter end - simulation gen 
+toc_sim = time.perf_counter()
+ttot_sim= round(toc_sim - tic_sim,3)
+print(f'Time taken for assigning SLD values: {ttot_sim} s')
+print(f'-------------------------------------------------')
 
-print(part_str + 'Calculating scattering function')
+print(f'-------------------------------------------------')
+print(part_str + 'Calculating SAS pattern')
+print('')
+# time counter start - SAS calculation
+tic_scatt = time.perf_counter()
 if scatt_cal_run==1:
     # scatt_cal xml
     print(info_str + 'Generating scatt_cal.xml')
@@ -113,3 +148,20 @@ if scatt_cal_run==1:
     subprocess.run(["python", script_path], cwd=working_dir, stderr=subprocess.PIPE)
 else:
     print(info_str + 'Calculation of scattring function not attempted')
+# time counter end - SAS calculation
+toc_scatt = time.perf_counter()
+ttot_scatt= round(toc_scatt - tic_scatt,3)
+print(f'Time taken for calculating SAS pattern: {ttot_sim} s')
+print(f'-------------------------------------------------')
+
+
+# Time satistics
+print('')
+print(f'-------------------------------------------------')
+print(f'Time statistics')
+print(f'-------------------------------------------------')
+print(f'Mesh generation :{ttot_sg} s')
+print(f'SLD assign      :{ttot_sim} s')
+print(f'SAS calculation :{ttot_scatt} s')
+print(f'Total           :{ttot_sg + ttot_sim + ttot_scatt} s')
+print(f'-------------------------------------------------')
--- bib_gen.py
+++ bib_gen.py
@@ -1,7 +1,19 @@
+"""
+This generates data for ball offcenter in box model
+data is saved in data folder
+
+Author: Arnab Majumdar
+Date: 24.06.2025
+"""
 from lib import xml_gen
 import subprocess
 import numpy as np
 import os
+import time
+
+# ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
 
 """
 input values
@@ -26,22 +38,23 @@ plt_node=False
 plt_cell=False
 plt_mesh=False
 
-### sim_gen ###
+### sim gen ###
 sim_model='bib'
-t_end=0
 dt=1
+t_end=0
 n_ensem=1
 
 ### model_param ###
-rad=15
+rad=10
 sld_in=2
 sld_out=1
+qclean_sld=0
 
 ### scatt_cal ###
 num_cat=3
 method_cat='extend'
 sassena_exe= '/home/amajumda/Documents/Softwares/sassena/compile/sassena'
-mpi_procs=2
+mpi_procs=4
 num_threads=2
 sig_file='signal.h5'
 scan_vec=np.array([1, 0, 0])
@@ -57,10 +70,14 @@ scatt_cal_run=1
 """
 xml gen and run scripts
 """
-part_str='Part>>>'
-info_str='Info>>>'
+part_str='Part>>>  '
+info_str='Info>>>  '
 
-print(part_str + 'Structure generation')
+print(f'-------------------------------------------------')
+print(part_str + 'Generating mesh')
+print('')
+# time counter start - struct gen
+tic_sg = time.perf_counter()
 if struct_gen_run==1:
     # generate xml for struct_gen
     print(info_str + 'Generating struct_gen.xml')
@@ -77,8 +94,17 @@ if struct_gen_run==1:
     subprocess.run(["python", script_path], cwd=working_dir, stderr=subprocess.PIPE)
 else:
     print(info_str + 'structure generation not attempted')
+# time counter end - struct gen 
+toc_sg = time.perf_counter()
+ttot_sg= round(toc_sg - tic_sg,3)
+print(f'Time taken for generating mesh: {ttot_sg} s')
+print(f'-------------------------------------------------')
 
-print(part_str + 'Simulating')
+print(f'-------------------------------------------------')
+print(part_str + 'Assigning SLD values to nodes')
+print('')
+# time counter start - simulation gen
+tic_sim = time.perf_counter()
 if sim_run==1:
     # simulation xml
     print(info_str + 'Generating simulation.xml')
@@ -86,7 +112,7 @@ if sim_run==1:
 
     # model xml
     print(info_str + 'Generating model_bib.xml')
-    xml_gen.model_bib_xml_write(xml_dir, rad, sld_in, sld_out)
+    xml_gen.model_bib_xml_write(xml_dir, rad, sld_in, sld_out, qclean_sld)
 
     # generate structure
     print(info_str + 'Executing sim_gen.py')
@@ -95,8 +121,17 @@ if sim_run==1:
     subprocess.run(["python", script_path], cwd=working_dir, stderr=subprocess.PIPE)
 else:
     print(info_str + 'simulation not attempted')
+# time counter end - simulation gen 
+toc_sim = time.perf_counter()
+ttot_sim= round(toc_sim - tic_sim,3)
+print(f'Time taken for assigning SLD values: {ttot_sim} s')
+print(f'-------------------------------------------------')
 
-print(part_str + 'Calculating scattering function')
+print(f'-------------------------------------------------')
+print(part_str + 'Calculating SAS pattern')
+print('')
+# time counter start - SAS calculation
+tic_scatt = time.perf_counter()
 if scatt_cal_run==1:
     # scatt_cal xml
     print(info_str + 'Generating scatt_cal.xml')
@@ -112,3 +147,20 @@ if scatt_cal_run==1:
     subprocess.run(["python", script_path], cwd=working_dir, stderr=subprocess.PIPE)
 else:
     print(info_str + 'Calculation of scattring function not attempted')
+# time counter end - SAS calculation
+toc_scatt = time.perf_counter()
+ttot_scatt= round(toc_scatt - tic_scatt,3)
+print(f'Time taken for calculating SAS pattern: {ttot_sim} s')
+print(f'-------------------------------------------------')
+
+
+# Time satistics
+print('')
+print(f'-------------------------------------------------')
+print(f'Time statistics')
+print(f'-------------------------------------------------')
+print(f'Mesh generation :{ttot_sg} s')
+print(f'SLD assign      :{ttot_sim} s')
+print(f'SAS calculation :{ttot_scatt} s')
+print(f'Total           :{ttot_sg + ttot_sim + ttot_scatt} s')
+print(f'-------------------------------------------------')
