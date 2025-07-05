f1b01946d52e8c532c7cb3ea3e600ad6317748d3
Arnab Majumdar <arnabmajumdar9@gmail.com>
2025-07-05 09:40:10 +0200

    added pylint suggestions

--- models/box/box_gen.py
+++ models/box/box_gen.py
@@ -5,33 +5,39 @@ data is saved in data folder
 Author: Arnab Majumdar
 Date: 24.06.2025
 """
-
-from lib import xml_gen
-import subprocess
-import numpy as np
 import os
+import sys
+import subprocess
 import time
+import warnings
+import numpy as np
+
+# find current dir and and ..
+lib_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+# add .. in path
+sys.path.append(lib_dir)
+# lib imports
+from lib import xml_gen # pylint: disable=import-error, wrong-import-position
 
 # ignore warnings
-import warnings
 warnings.filterwarnings("ignore")
 
-"""
-input values
-"""
+##############
+# input values
+##############
 # script dir and working dir
 script_dir = "src"  # Full path of the script
 working_dir = "."  # Directory to run the script in
 
 
 ### struct gen ###
-xml_dir='./xml' 
+xml_dir='./xml'
 length_a=40.
-length_b=40. 
+length_b=40.
 length_c=40.
-nx=40 
-ny=40 
-nz=40 
+nx=40
+ny=40
+nz=40
 el_type='lagrangian'
 el_order=1
 update_val=True
@@ -72,7 +78,7 @@ xml gen and run scripts
 part_str='Part>>>  '
 info_str='Info>>>  '
 
-print(f'-------------------------------------------------')
+print('-------------------------------------------------')
 print(part_str + 'Generating mesh')
 print('')
 # time counter start - struct gen
@@ -80,26 +86,26 @@ tic_sg = time.perf_counter()
 if struct_gen_run==1:
     # generate xml for struct_gen
     print(info_str + 'Generating struct_gen.xml')
-    xml_gen.struct_xml_write(xml_dir, 
-                            length_a, length_b, length_c, 
-                            nx, ny, nz, 
-                            el_type, el_order,  
-                            update_val, plt_node, plt_cell, plt_mesh)
+    xml_gen.struct_xml_write(xml_dir,
+                              length_a, length_b, length_c,
+                                nx, ny, nz,
+                                  el_type, el_order,
+                                    update_val, plt_node, plt_cell, plt_mesh)
 
     # generate structure
     print(info_str + 'Executing struct_gen.py')
     script_path = os.path.join(script_dir, 'struct_gen.py')  # Full path of the script
     #working_dir = "."  # Directory to run the script in
-    subprocess.run(["python", script_path], cwd=working_dir, stderr=subprocess.PIPE)
+    subprocess.run(["python", script_path], cwd=working_dir, stderr=subprocess.PIPE, check=True)
 else:
     print(info_str + 'structure generation not attempted')
-# time counter end - struct gen 
+# time counter end - struct gen
 toc_sg = time.perf_counter()
 ttot_sg= round(toc_sg - tic_sg,3)
 print(f'Time taken for generating mesh: {ttot_sg} s')
-print(f'-------------------------------------------------')
+print('-------------------------------------------------')
 
-print(f'-------------------------------------------------')
+print('-------------------------------------------------')
 print(part_str + 'Assigning SLD values to nodes')
 print('')
 # time counter start - simulation gen
@@ -117,16 +123,16 @@ if sim_run==1:
     print(info_str + 'Executing sim_gen.py')
     script_path = os.path.join(script_dir, 'sim_gen.py')  # Full path of the script
     #working_dir = "."  # Directory to run the script in
-    subprocess.run(["python", script_path], cwd=working_dir, stderr=subprocess.PIPE)
+    subprocess.run(["python", script_path], cwd=working_dir, stderr=subprocess.PIPE, check=True)
 else:
     print(info_str + 'simulation not attempted')
-# time counter end - simulation gen 
+# time counter end - simulation gen
 toc_sim = time.perf_counter()
 ttot_sim= round(toc_sim - tic_sim,3)
 print(f'Time taken for assigning SLD values: {ttot_sim} s')
-print(f'-------------------------------------------------')
+print('-------------------------------------------------')
 
-print(f'-------------------------------------------------')
+print('-------------------------------------------------')
 print(part_str + 'Calculating SAS pattern')
 print('')
 # time counter start - SAS calculation
@@ -143,23 +149,23 @@ if scatt_cal_run==1:
     print(info_str + 'Executing scatt_cal.py')
     script_path = os.path.join(script_dir, 'scatt_cal.py')  # Full path of the script
     #working_dir = "."  # Directory to run the script in
-    subprocess.run(["python", script_path], cwd=working_dir, stderr=subprocess.PIPE)
+    subprocess.run(["python", script_path], cwd=working_dir, stderr=subprocess.PIPE, check=True)
 else:
     print(info_str + 'Calculation of scattring function not attempted')
 # time counter end - SAS calculation
 toc_scatt = time.perf_counter()
 ttot_scatt= round(toc_scatt - tic_scatt,3)
 print(f'Time taken for calculating SAS pattern: {ttot_sim} s')
-print(f'-------------------------------------------------')
+print('-------------------------------------------------')
 
 
 # Time satistics
 print('')
-print(f'-------------------------------------------------')
-print(f'Time statistics')
-print(f'-------------------------------------------------')
+print('-------------------------------------------------')
+print('Time statistics')
+print('-------------------------------------------------')
 print(f'Mesh generation :{ttot_sg} s')
 print(f'SLD assign      :{ttot_sim} s')
 print(f'SAS calculation :{ttot_scatt} s')
 print(f'Total           :{ttot_sg + ttot_sim + ttot_scatt} s')
-print(f'-------------------------------------------------')
+print('-------------------------------------------------')
