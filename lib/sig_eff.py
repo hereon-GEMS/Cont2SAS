13a36432d4df9454054c887f982600be570b5841
Arnab Majumdar <arnabmajumdar9@gmail.com>
2025-08-04 16:47:38 +0200

    added license text above each file

---
+++ lib/sig_eff.py
@@ -0,0 +1,25 @@
+# One line to give the program's name and an idea of what it does.
+# Copyright (C) 2025  Arnab Majumdar
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see
+# <https://www.gnu.org/licenses/>.
+
+"""
+New files related to sig eff calculations
+required for further development
+can be added in the file.
+
+Creator: Arnab Majumdar
+Date 04.08.2025
+"""
