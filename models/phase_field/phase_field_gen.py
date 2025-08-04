015760d0165acfaa7508a6d373d8108add13eefa
Arnab Majumdar <arnabmajumdar9@gmail.com>
2025-08-04 13:01:41 +0200

    made params consistent

--- models/phase_field/phase_field_gen.py
+++ models/phase_field/phase_field_gen.py
@@ -75,9 +75,9 @@ for t_idx, sim_time in enumerate(sim_times):
     ### scatt_cal ###
     num_cat=501 # use 3 for the categorization picture
     method_cat='extend'
-    sassena_exe= '/home/amajumda/Documents/Softwares/sassena/compile/sassena'
-    mpi_procs=4
-    num_threads=2
+    sassena_exe= './Sassena/Sassena.AppImage'
+    mpi_procs=1
+    num_threads=1
     sig_file='signal.h5'
     scan_vec=np.array([1, 0, 0])
     Q_range=np.array([2*np.pi/length_a, 1])
