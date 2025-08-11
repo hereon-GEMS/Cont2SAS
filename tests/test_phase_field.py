a4138612e98034701e57394fd65fda75faa4d3af
Arnab Majumdar <arnabmajumdar9@gmail.com>
2025-08-11 15:12:20 +0200

    pulled changes from main

--- tests/test_phase_field.py
+++ tests/test_phase_field.py
@@ -35,10 +35,9 @@ def test_phase_field_gen():
     1. Check whether phase_field_gen runs
     """
     gen_result = subprocess.run(
-        ["python", "models/phase_field/phase_field_gen.py"],
-        capture_output=True,
-        text=True,
-        check=True
+        ["python", "models/phase_field/phase_field_gen.py"], 
+        capture_output=True, 
+        text=True
     )
     # Ensure it runs without crashing
     assert gen_result.returncode == 0, "phase_field model generates data"
@@ -49,10 +48,9 @@ def test_phase_field_plot():
     1. Check whether phase_field_plot runs
     """
     plot_result = subprocess.run(
-        ["python", "models/phase_field/phase_field_plot.py"],
-        capture_output=True,
-        text=True,
-        check=True
+        ["python", "models/phase_field/phase_field_plot.py"], 
+        capture_output=True, 
+        text=True
     )
     # Ensure it runs without crashing
     assert plot_result.returncode == 0, "phase_field model plots data"
@@ -62,6 +60,7 @@ def test_check_phase_field_plot():
     Test script for $C2S_HOME/models/phase_field/phase_field_plot.py:
     1. Check whether phase_field_plot plots IvsQ figure
     """
+    gold_img='figure/gold/Iq_phase_field.pdf'
     plt_img='figure/phase_field/Iq_phase_field.pdf'
     if_exist=os.path.isfile(plt_img)
     # Ensure plotted file exist
@@ -75,7 +74,7 @@ def test_compare_phase_field_plot():
     gold_img='figure/gold/Iq_phase_field.pdf'
     plt_img='figure/phase_field/Iq_phase_field.pdf'
     # Check expected output
-    assert comp_img(plt_img, gold_img, tol=2) is None
+    assert comp_img(plt_img, gold_img, tol=2)==None
 
 def test_clean_up():
     """
@@ -83,7 +82,7 @@ def test_clean_up():
     1. Removes created data and figure
     2. Checks whether removed
     """
-    data_dir='data/250_250_250_100_100_100_lagrangian_1'
+    data_dir='data/250p0_250p0_250p0_100_100_100_lagrangian_1'
     fig_dir='figure/phase_field'
     # remove data files
     shutil.rmtree(data_dir)
@@ -92,3 +91,4 @@ def test_clean_up():
     # check if it is removed
     assert os.path.isdir(data_dir)==0
     assert os.path.isdir(fig_dir)==0
+
