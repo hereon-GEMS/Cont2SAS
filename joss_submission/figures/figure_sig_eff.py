c3d018aeba6bf24f65b3498c6bd29e9dede0dce0
Arnab Majumdar <arnab.majumdar@hereon.de>
2026-06-19 14:44:22 +0200

    minor adjustment in fi2 left subfig

--- joss_submission/figures/figure_sig_eff.py
+++ joss_submission/figures/figure_sig_eff.py
@@ -36,7 +36,7 @@ def create_pdf_figure(pdf_paths, output_image='figure.png', dpi=150):
         images.append(pages[0])  # use first page only
 
     cols = len(images)
-    fig, axes = plt.subplots(1, cols, figsize=(5 * cols, 5))
+    fig, axes = plt.subplots(1, cols, figsize=(6 * cols, 4))
 
     if cols == 1:
         axes = [axes]
--- models/sld_grow/sld_grow_plot_joss.py
+++ models/sld_grow/sld_grow_plot_joss.py
@@ -274,7 +274,7 @@ arrow = FancyArrowPatch(
     zorder=100
 )
 ax_scatt_all.add_patch(arrow)
-ax_scatt_all.text(1.23e-2, 4.2e5, "time", fontsize=17, ha="center", va="center", zorder=100,
+ax_scatt_all.text(1.27e-2, 4.2e5, "time", fontsize=17, ha="center", va="center", zorder=100,
                    bbox=dict(facecolor="yellow", edgecolor="k", boxstyle="round,pad=0.2"))
 # plot formatting
 ## legend
